85d4e4a92ff290b226314b0843ba83173a10daf0
ibrahim <98467166+Ibramadi75@users.noreply.github.com>
2024-01-07 13:38:27 +0100

    Update Bumpers.s

--- src/Bumpers.s
+++ src/Bumpers.s
@@ -53,7 +53,7 @@ RIGHT_BUMPER_CT_ADR    EQU     RIGHT_BUMPER_BASE + BUMPER_CT_OFFSET
 			
 		IMPORT INCR_VAL_STORED_IN_R1_ADR
 
-;; Met les valeurs des adresses liées à l'usage des bumpers à 0
+;; Put to 0 values of bumper's dedicated adress
 INIT_BUMPERS_ADRESS
 		MOV r0, #0
 		; Charger l'adresse de LEFT_BUMPER_ST_ADR et stocker 0
@@ -74,7 +74,7 @@ INIT_BUMPERS_ADRESS
 		
 		BX LR
 
-;; Initialisation des bumpers
+;; Init bumpers and their adress
 BUMPERS_INIT
 		LDR r7, = GPIO_PORTE_BASE + GPIO_I_PUR
 		
@@ -99,38 +99,41 @@ HANDLE_BUMPERS_INTERACTIONS
 		PUSH {LR}
 		BL BUMPER_DROIT_READSTATE
 		
-		LDR r1, =RIGHT_BUMPER_ST_ADR    ; Charger l'adresse de RIGHT_BUMPER_ST_ADR dans r1
+		LDR r1, =RIGHT_BUMPER_ST_ADR    ; Load RIGHT_BUMPER_ST_ADR in r1
 		LDR r2, [r1]    
 		
 		CMP r2, #1
 		BNE CHECK_NEXT_BUMPER
 		
-		;; sinon éteint led et incrémente r8
+		;; put led of to indicate user can't configure
 		BL LED_DROITE_OFF;
-		
+
+  		;; increment right bumper counter
 		BL INCR_RIGHT_BUMPER_COUNTER
 			
-		;; délai de sécurité pour éviter le spam machine
+		;; security delay to prevent spam
 		MOV r5, #1
 		BL WAIT_R5_SECONDS
 		
-		;; allumage de la led pour indiquer que l'utilisateur peut de nouveau incrémenter
+		;; put led on to indicate user can now continue to configure
 		BL LED_DROITE_ON;
 		
 CHECK_NEXT_BUMPER
 		BL BUMPER_GAUCHE_READSTATE
 		
-		LDR r1, =LEFT_BUMPER_ST_ADR    ; Charger l'adresse de LEFT_BUMPER_ST_ADR dans r1
+		LDR r1, =LEFT_BUMPER_ST_ADR    ; Load LEFT_BUMPER_ST_ADR in r1
 		LDR r2, [r1] 
 		
 		CMP r2, #1
 		BNE LEFT_BUMPER_NOT_PRESSED
 		
-		;; sinon éteint led et incrémente r8
+		;; put led of to indicate user can't configure
  		BL LED_GAUCHE_OFF;
-		
+
+  		;; increment left bumper counter
 		BL INCR_LEFT_BUMPER_COUNTER
-		
+
+		;; security delay to prevent spam
 		MOV r5, #1
 		BL WAIT_R5_SECONDS
 		
@@ -141,9 +144,9 @@ LEFT_BUMPER_NOT_PRESSED
 		
 		BX LR
 
-;; Lis l'état du bumper de gauche
+;; Lis l'Ã©tat du bumper de gauche
 BUMPER_GAUCHE_READSTATE
-		LDR r2, =LEFT_BUMPER_ST_ADR ;; charge l'adresse dédiée à la récupération de l'état du bumper
+		LDR r2, =LEFT_BUMPER_ST_ADR ;; load adress dedicated to left bumper's state
 		LDR r1, =GPIO_PORTE_BASE + (BROCHE1<<2)
 		
 		PUSH {LR}
@@ -152,9 +155,9 @@ BUMPER_GAUCHE_READSTATE
 		
 		BX LR
 
-;; Lis l'état du bumper de droite
+;; Lis l'Ã©tat du bumper de droite
 BUMPER_DROIT_READSTATE
-		LDR r2, =RIGHT_BUMPER_ST_ADR ;; charge l'adresse dédiée à la récupération de l'état du bumper
+		LDR r2, =RIGHT_BUMPER_ST_ADR ;; load adress dedicated to right bumper's state
 		LDR r1, =GPIO_PORTE_BASE + (BROCHE0<<2)
 
 DEFINE_STATE
